42097aba059014f218a4d00b703ea08b1f4ab7c9
M.D. van Es <es@mdworld.nl>
2018-11-19 16:12:48 +0100

    Refactor authCallback

--- serverless/twitter.js
+++ serverless/twitter.js
@@ -6,7 +6,27 @@ const OAuth = require('oauth');
 const { SCREEN_NAME, TWEET_COUNT } = process.env;
 const GET_RECENT_TWEETS_URL = `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=${SCREEN_NAME}&count=${TWEET_COUNT}`;
 
-// TODO remove hard coded tokens (possible to use non-oauth version?)
+const authCallback = (callback, headers) => {
+  /* params: error, data, result */
+  return (error, data) => {
+    try {
+      if (error) {
+        console.log(error);
+        throw new Error(`Auth failure GET_RECENT_TWEETS_URL ${error}`);
+      }
+      callback(null, {
+        statusCode: 200,
+        headers,
+        body: JSON.stringify(data),
+      });
+    } catch (err) {
+      console.log(err);
+      callback(`Failed GET_RECENT_TWEETS_URL ${err}`);
+    }
+  };
+};
+
+// TODO remove hard coded tokens
 module.exports.getRecentTweets = (event, context, callback) => {
   const headers = util.safeGetHeaders(event.headers.origin);
   const oauth = new OAuth.OAuth(
@@ -19,28 +39,10 @@ module.exports.getRecentTweets = (event, context, callback) => {
     'HMAC-SHA1'
   );
 
-  /* params: error, data, result */
-  const authCallback = (error, data) => {
-    try {
-      if (error) {
-        throw new Error(`Auth failure GET_RECENT_TWEETS_URL ${error}`);
-      }
-      console.log('result after authCallback', JSON.stringify(data))
-      callback(null, {
-        statusCode: 200,
-        headers,
-        body: JSON.stringify(data),
-      });
-    } catch (err) {
-      console.log(err);
-      callback(`Failed GET_RECENT_TWEETS_URL ${err}`);
-    }
-   };
-
   oauth.get(
     GET_RECENT_TWEETS_URL,
     '132144715-JB0dtp503oGA0ArDsZ0r4oFsh9GcaQRAvc1Xqyyw',
     '9wCdiAuuzIWFdeAE6gL4hzjV5Rsj1ZLQzyjFC5aKDjHMN',
-    authCallback
+    authCallback(callback, headers)
   );
 };
